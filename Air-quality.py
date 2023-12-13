2936f438dcace1dcc50e013e565311a85a0172ee
Yetpet <50325572+Yetpet@users.noreply.github.com>
2023-12-13 21:02:00 +0000

    save files

--- Air-quality.py
+++ Air-quality.py
@@ -4,7 +4,6 @@ import numpy as np
 import pandas as pd
 import joblib as jb
 from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import MinMaxScaler
 from datetime import datetime
 import folium
 import pickle
@@ -28,10 +27,26 @@ def run():
     """
     )
 
-    # Load model 
-    # model = jb.load('mgbr_model.pkl')
-    with open('mgbr_model.pkl', 'rb') as file:
-        model = pickle.load(file)
+    # Assuming 'scaler' is already fitted on the training data
+    global scaler_fitted 
+    scaler_fitted = False  # Flag to check if the scaler is fitted
+
+    # Load the trained model and scaler
+    try:
+        with open('mgbr_model.pkl', 'rb') as model_file:
+            model = pickle.load(model_file)
+            print(type(model))  # Add this line to check the type of 'model'
+    except FileNotFoundError:
+        st.warning("Model not found. Please retrain the model.")
+
+    try:
+        with open('scaler.pkl', 'rb') as scaler_file:
+            scaler = pickle.load(scaler_file)
+        scaler_fitted = True
+    except FileNotFoundError:
+        st.warning("Scaler not found. Please retrain the model to generate a fitted scaler.")
+        scaler_fitted = False
+
 
     # Placeholder DataFrame for state labels
     state_labels_df = pd.DataFrame({
@@ -48,6 +63,12 @@ def run():
 
     # Function to preprocess input and make predictions
     def make_predictions(lat, lon, date, state_name):
+        # global scaler_fitted  # Declare it as global to modify the global variable
+
+        if not scaler_fitted:
+            st.warning("Scaler is not fitted. Please retrain the model to generate a fitted scaler.")
+            return np.zeros(9)  # Return zeros or any default values as needed
+
         # Feature engineering based on date
         date = datetime.strptime(str(date), "%Y-%m-%d")
         day = date.day
@@ -69,21 +90,34 @@ def run():
             'hour': [hour],
             'state_label': [state_label]
         })
+
+        columns_to_normalize = ['lon', 'lat', 'month', 'day', 'hour','state_label']
+
+        # Extract the features to be normalized
+        # features_to_normalize = nigeria_air_pollution[columns_to_normalize]
         
-        scaler = MinMaxScaler()
-        normalized_features = pd.DataFrame(scaler.transform(input_features), columns=columns_to_normalize)
+        # Normalize the input features using the fitted scaler
+        #input_features = np.array([[lon, lat, month, day, hour, state_label]])  # Convert to NumPy array
+        #normalized_features = pd.DataFrame(scaler.fit_transform(input_features), columns=columns_to_normalize)
+        #normalized_features = scaler.transform(input_features)
+        # normalized_features = pd.DataFrame(scaler.transform(input_features), columns=columns_to_normalize).to_numpy()
 
         # Combine the normalized features with other non-normalized columns
-        input_data = pd.concat([input_features.drop(columns=columns_to_normalize), normalized_features], axis=1)
+        #input_data = pd.DataFrame(normalized_features, columns=columns_to_normalize)
+
+
+        # Combine the normalized features with other non-normalized columns
+        # input_data = pd.concat([input_features.drop(columns=columns_to_normalize), normalized_features], axis=1)
+        new_data = pd.DataFrame([[7.5153071,5.454095299, 12,15,21, 37]], columns=['lon', 'lat', 'month', 'day', 'hour','state_label'])
 
         # Make predictions using the trained model
-        predictions = model.predict(input_data)
+        predictions = model.predict(new_data)
 
 
         #predictions = np.random.rand(9)  # Dummy values for demonstration
 
-        #return predictions
-        return predictions.flatten()  # Flatten the predictions to a 1D array
+        return predictions
+        #return predictions.flatten()  # Flatten the predictions to a 1D array
 
     # Function to create a folium map
     def create_map(lat, lon, predictions):
@@ -118,53 +152,6 @@ def run():
         st.write("### Air Quality Map:")
         folium_map = create_map(lat, lon, predictions)
         st.write(folium_map)
-
-    # # Load the pre-trained model
-    # gb_model = jb.load('gb_model.joblib')
-
-    # # Streamlit UI components for user input
-    # st.title('Air Quality Prediction')
-    # co = st.number_input('Enter CO Level:')
-    # no = st.number_input('Enter NO Level:')
-    # no2 = st.number_input('Enter NO2 Level:')
-    # o3 = st.number_input('Enter O3 Level:')
-    # so2 = st.number_input('Enter SO2 Level:')
-    # pm2_5 = st.number_input('Enter PM2.5 Level:')
-    # pm10 = st.number_input('Enter PM10 Level:')
-    # nh3 = st.number_input('Enter NH3 Level:')
-    # lon = st.number_input('Enter Longitude:')
-    # lat = st.number_input('Enter Latitude:')
-    # month = st.number_input('Enter Month:')
-    # day = st.number_input('Enter Day:')
-    # hour = st.number_input('Enter Hour:')
-    # state_label = st.number_input('Enter State Name:')
-
-
-    # # Create a DataFrame from user inputs
-    # user_data = pd.DataFrame({
-    #     'co': [co],
-    #     'no': [no],
-    #     'no2': [no2],
-    #     'o3': [o3],
-    #     'so2': [so2],
-    #     'pm2_5': [pm2_5],
-    #     'pm10': [pm10],
-    #     'nh3': [nh3],
-    #     'lon': [lon],
-    #     'lat': [lat],
-    #     'month': [month],
-    #     'day': [day],
-    #     'hour': [hour],
-    #     'state_label': [state_label]
-    # })
-
-    # # Make predictions using the loaded model
-    # # user_data = pd.DataFrame({'co': [co], 'no': [no], 'other_features': [...]})
-    # prediction = gb_model.predict(user_data)
-
-    # # Display the prediction
-    # st.write(f'Predicted AQI: {prediction[0]}')
-
 
 
 if __name__ == "__main__":
